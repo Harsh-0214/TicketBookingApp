c28149afffaad1eff85926cfd8d8827fa523c132
harsh2003 <harsh_2003@hotmail.com>
2024-04-01 21:10:01 -0400

    update pom file

--- TicketBookingApp/src/main/java/com/ticketbookingapp/App.java
+++ TicketBookingApp/src/main/java/com/ticketbookingapp/App.java
@@ -4,7 +4,6 @@ public class App {
     public String getGreeting(){
         return "Hello World!";
     }
-
     public static void main( String[] args ){
         System.out.println(new App().getGreeting());
     }
